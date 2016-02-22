c18ba954e2803070195613f8a525c6f61778134b
c.bejnar <bejnar@gmail.com>
2016-02-22 01:26:18 -0500

    Sample Commit.
    Doing a wercker quickstart.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({cities : ["San Francisco","Amsterdam", "Berlin", "New York", "Palo ALto", "San Mateo"]}));
-    res.end();
+app.get('/', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.write(JSON.stringify({
+    cities: [
+      "Dubrovnik",
+      "Berlin",
+      "New York",
+      "Pittsburgh",
+      "Philadelphia",
+      "Norwalk"
+    ]
+  }));
+  res.end();
 });
 
 var port = process.env.PORT || 5000;
